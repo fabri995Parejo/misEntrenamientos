97e1ac36529cfa3fa8bc4fb1b8cf3801d35f7ecf
Ivan Fabricio Parejo <fabricioparejo995@gmail.com>
2024-06-22 16:26:36 -0300

    Modularizo valorDolarScript para que quede aun mas limpio el codigo

--- src/main/java/com/utilidades/valorDolarScript.java
+++ src/main/java/com/utilidades/valorDolarScript.java
@@ -8,29 +8,52 @@ import java.util.List;
 
 public class valorDolarScript {
     public static List<String> mostrarDolar() throws IOException, InterruptedException{
-        //inicializo a traves de metodo
+        
+        //inicializo el script de nodejs a traves de metodo
         Process process = inicializarScript();
-
-            
+    
             
         // Leer la salida del script Node.js
-            
-        //Se crea un BufferedReader para leer la salida estándar (stdout) del proceso
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
         List<String> outputLines = new ArrayList<>();
+        leerSalidaScript(outputLines, process);
+         
+        //Leo posibles errores y los imprimo en pantalla
+        leerEImprimirErrores(process);
+        
+        return outputLines;
+    }
+
+    private static Process inicializarScript() throws IOException, InterruptedException{
+        // Construir el comando para ejecutar el script Node.js
+
+        //se crea un array de String que contiene el comando a ejecutar (node index.js)
+        String[] command = {"node", "/home/fabricio/Documentos/vscode/Entrenamientos/src/scraping/index.js"};
+        
+        
+        //Runtime.getRuntime().exec(command) para ejecutar el comando
+        return Runtime.getRuntime().exec(command);
+
+    }
+
+    private static void leerSalidaScript(List<String> outputList, Process proceso) throws IOException, InterruptedException{ 
+        //Se crea un BufferedReader para leer la salida estándar (stdout) del proceso
+        BufferedReader reader = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+        String line;
+    
         //El bucle while lee cada línea de la salida del proceso y la imprime en la consola 
         while ((line = reader.readLine()) != null) {
-            outputLines.add(line);
+            outputList.add(line);
         }
+    }
 
+    private static void leerEImprimirErrores(Process proceso) throws IOException, InterruptedException{
         // Esperar a que el proceso termine y capturar errores
-
+        String line;
         //process.waitFor() espera y devuelve el código de salida del proceso.
-        int exitCode = process.waitFor();
+        int exitCode = proceso.waitFor();
         if (exitCode != 0) {
             //Si el código de salida no es 0, indica que hubo un error en la ejecución del script Node.js 
-            BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(proceso.getErrorStream()));
             //se crea otro BufferedReader para leer la salida de error estándar (stderr) del proceso.
             List<String> errorLines = new ArrayList<>();
             //Este bucle while lee caida línea de la salida de error y la imprime 
@@ -44,18 +67,5 @@ public class valorDolarScript {
             }
                 
         }
-        return outputLines;
-    }
-
-    public static Process inicializarScript() throws IOException, InterruptedException{
-        // Construir el comando para ejecutar el script Node.js
-
-        //se crea un array de String que contiene el comando a ejecutar (node index.js)
-        String[] command = {"node", "/home/fabricio/Documentos/vscode/Entrenamientos/src/scraping/index.js"};
-        
-        
-        //Runtime.getRuntime().exec(command) para ejecutar el comando
-        return Runtime.getRuntime().exec(command);
-
     }
 }
